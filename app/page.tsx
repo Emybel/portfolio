93d051dfa1cf37f3612a63457bd1e93ff68e285c
Imane BELAID <31738219+Emybel@users.noreply.github.com>
2024-01-16 14:36:56 +0100

    Add section-devider

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Intro from "./components/intro";
+import SectionDevider from "./components/section-devider";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center px-4"> 
       <Intro/>
+      <SectionDevider/>
     </main>
   )
 }
