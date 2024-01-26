654d4b5f4fa57355c4be892edc7178e90ac6ec79
Imane BELAID <31738219+Emybel@users.noreply.github.com>
2024-01-26 21:56:57 +0100

    Create experience component

--- app/components/experience.tsx
+++ app/components/experience.tsx
@@ -1,31 +1,61 @@
-"use client"
+"use client";
 
-import React from 'react';
-import SectionHeading from './section-heading';
-import { VerticalTimeline, VerticalTimelineElement } from "react-vertical-timeline-component";
-  import "react-vertical-timeline-component/style.min.css";
-import { experiencesData } from '@/lib/data';
+import React from "react";
+import SectionHeading from "./section-heading";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import { experiencesData } from "@/lib/data";
+import { useSectionInView } from "@/lib/hooks";
+import { InView } from "react-intersection-observer";
 
-function Experience() {
+
+export default function Experience() {
+  const { ref } = useSectionInView("Experience");
+  // const { theme } = useTheme();
+
   return (
-    <section id='experience'>
-        <SectionHeading>My experience</SectionHeading> 
-        <VerticalTimeline>
-                {
-                    experiencesData.map((item,index) =>(
-                        <React.Fragment key={index}>
-                            <VerticalTimelineElement>
-                                <h3>{item.title}</h3>
-                                <p>{item.location}</p>
-                                <p>{item.description}</p>
-                            </VerticalTimelineElement>
-                        </React.Fragment>
-                        
-                    ))
-                }
-        </VerticalTimeline>
+    <section id="experience" ref={ref} className="scroll-mt-28 mb-28 sm:mb-40">
+      <SectionHeading>My experience</SectionHeading>
+      <VerticalTimeline
+       lineColor="">
+        {experiencesData.map((item, index) => (
+          <React.Fragment key={index}>
+            <VerticalTimelineElement
+            visible={InView}
+              contentStyle={{
+                // background:
+                //   theme === "light" ? "#f3f4f6" : "rgba(255, 255, 255, 0.05)",
+                boxShadow: "none",
+                border: "1px solid rgba(0, 0, 0, 0.05)",
+                textAlign: "left",
+                padding: "1.3rem 2rem",
+              }}
+              contentArrowStyle={{
+                borderRight: "0.4rem solid #9ca3af"
+                  // theme === "light"
+                  //   ? "0.4rem solid #9ca3af"
+                  //   : "0.4rem solid rgba(255, 255, 255, 0.5)",
+              }}
+              date={item.date}
+              icon={item.icon}
+              iconStyle={{
+                background: "rgba(255, 255, 255, 0.15)",
+                  // theme === "light" ? "white" : "rgba(255, 255, 255, 0.15)",
+                fontSize: "1.5rem",
+              }}
+            >
+              <h3 className="font-semibold capitalize">{item.title}</h3>
+              <p className="font-normal !mt-0">{item.location}</p>
+              <p className="!mt-1 !font-normal text-gray-700 ">
+                {item.description}
+              </p>
+            </VerticalTimelineElement>
+          </React.Fragment>
+        ))}
+      </VerticalTimeline>
     </section>
-  )
+  );
 }
-
-export default Experience
