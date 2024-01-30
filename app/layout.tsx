bcbea758f6b3a216af2493c2905ebad65bef4895
Imane BELAID <31738219+Emybel@users.noreply.github.com>
2024-01-30 09:26:04 +0100

    Enhance design

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en" className='!scroll-smooth'>
       <body className={`${inter.className}
-      bg-gray-50 text-gray-950 relative h-[5000px] pt-28 sm:pt-36`
+      bg-gray-50 text-gray-950 relative pt-28 sm:pt-36`
     }>
       <div className='bg-[#f1d1fc] absolute top-[-6rem] right-[11rem] h-[31.25rem] -z-10 w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]'></div>
       <div className='bg-[#d1fce9] absolute top-[-1rem] -z-10 right-[35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]'></div>
