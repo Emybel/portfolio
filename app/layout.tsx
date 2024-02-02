0e95afc65264815cb2f3457d84c065549849a533
Imane BELAID <31738219+Emybel@users.noreply.github.com>
2024-02-02 12:03:26 +0100

    Create the footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,11 @@
-import ActiveSectionContextProvider from '@/context/active-section-context'
-import Header from './components/header'
-import './globals.css'
-import { Inter } from 'next/font/google'
-import { Toaster } from 'react-hot-toast'
+import ActiveSectionContextProvider from '@/context/active-section-context';
+import Header from '@/app/components/Header';
+import './globals.css';
+import { Inter } from 'next/font/google';
+import { Toaster } from 'react-hot-toast';
+import Footer from './components/footer';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Imane Belaid | Personal Portfolio',
@@ -21,12 +22,14 @@ export default function RootLayout({
       <body className={`${inter.className}
       bg-gray-50 text-gray-950 relative pt-28 sm:pt-36`
     }>
+      
       <div className='bg-[#f1d1fc] absolute top-[-6rem] right-[11rem] h-[31.25rem] -z-10 w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]'></div>
       <div className='bg-[#d1fce9] absolute top-[-1rem] -z-10 right-[35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]'></div>
       <ActiveSectionContextProvider>
         <Header />
         {children}
         <Toaster position='bottom-right' />
+        <Footer/>
       </ActiveSectionContextProvider>
       
       
