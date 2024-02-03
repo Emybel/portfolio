72f001980176759208c3c6922f792f1d2a283f95
Imane BELAID <31738219+Emybel@users.noreply.github.com>
2024-02-03 18:36:29 +0100

    Update colors in dark theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,8 +29,8 @@ export default function RootLayout({
         className={`${inter.className}
       bg-gray-50 dark:bg-gray-900 text-gray-950 dark:text-gray-50 dark:text-opacity-90  relative pt-28 sm:pt-36`}
       >
-        <div className="bg-[#f1d1fc] dark:bg-[#ae85bc] absolute top-[-6rem] right-[11rem] h-[31.25rem] -z-10 w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
-        <div className="bg-[#d1fce9] dark:bg-[#9ac8b4] absolute top-[-1rem] -z-10 right-[35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
+        <div className="bg-[#f1d1fc] dark:bg-[#603b6d] absolute top-[-6rem] right-[11rem] h-[31.25rem] -z-10 w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
+        <div className="bg-[#d1fce9] dark:bg-[#204f3b] absolute top-[-6rem] -z-10 right-[35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
 
         <ThemeContextProvider>
           <ActiveSectionContextProvider>
